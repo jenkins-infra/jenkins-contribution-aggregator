e6a8ccc3136d955f282cfb891b924f15ef905939
Jean-Marc Meessen <jean-marc@meessen-web.org>
2023-05-30 14:36:42 +0200

    Update "extract" on-line help

--- cmd/extract.go
+++ cmd/extract.go
@@ -30,13 +30,12 @@ import (
 // extractCmd represents the extract command
 var extractCmd = &cobra.Command{
 	Use:   "extract",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Extracts the top submitters from the supplied pivot table",
+	Long: `This command extract the top submitter for a given period.
+The input file is first validated before being processed.
+The output can be specified to a file. Per default it is directed to the
+standard output (console).
+`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("extract called")
 	},
